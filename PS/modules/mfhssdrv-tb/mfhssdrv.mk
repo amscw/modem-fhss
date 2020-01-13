92176a546a5b797cc726ef6e23f78c2d9f01e62f
amscw <moskwin.a.va@gmail.com>
2020-01-13 17:38:01 +0300

    set correct defines

--- PS/modules/mfhssdrv-tb/mfhssdrv.mk
+++ PS/modules/mfhssdrv-tb/mfhssdrv.mk
@@ -9,11 +9,11 @@ MFHSSDRV_SITE = /home/alex-m/projects/modem-fhss/PS/modules/mfhssdrv
 MFHSSDRV_SITE_METHOD = local
 MFHSSDRV_DEPENDENCIES = linux
 
-define MODEM_FHSS_MOD_BUILD_CMDS
+define MFHSSNET_BUILD_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) CC=$(TARGET_CC) -C $(@D) KERNELDIR=$(LINUX_DIR) modules
 endef
 
-define MODEM_FHSS_MOD_INSTALL_TARGET_CMDS
+define MFHSSNET_INSTALL_TARGET_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) CC=$(TARGET_CC) -C $(@D) KERNELDIR=$(LINUX_DIR) modules_install
 endef
 
